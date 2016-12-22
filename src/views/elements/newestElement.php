85326ddb229eb46272306e5c22ecc8b811a0a6d5
victorli5674 <victorli5674@yahoo.com>
2016-12-22 02:43:54 -0800

    made some changes

---
+++ src/views/elements/newestElement.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace VictorLi\hw3\views\elements;
+
+class newestElement extends Element {
+    public function render($data) {
+        ?><h3>Newest</h3>
+        <?php
+            if(empty($data)) {
+                ?>
+                    <p>No story found</p><br>
+                <?php
+            }
+            else {
+                ?>
+                    <ol>
+                <?php
+                    foreach($data as $Identifer=>$Title) {
+                        ?>
+                            <li><a href="index.php?c=ReadStory&m=invoke&arg1=<?php echo($Identifer); ?>"><?php echo($Title); ?></a></li>
+                        <?php
+                    }
+                ?>
+                    </ol>
+                <?php
+            }
+    }
+}
